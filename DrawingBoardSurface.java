f16a84cce4c39cf8fcb0bab79a4f9e918eee5fef
msilemsile <msilemsile@gmail.com>
2016-08-16 14:38:53 +0800

    update

--- DrawingBoardSurface.java
+++ DrawingBoardSurface.java
@@ -6,13 +6,17 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.LinkedList;
 
 /**
  * 简易画板
@@ -38,21 +42,27 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
     private int mDrawOperate;                       //当前绘制操作
     public static final int OPERATE_DRAW = 0;       //画笔操作
     public static final int OPERATE_CLEAR = -1;     //橡皮操作
+    public static final int OPERATE_BACK = 1;       //撤销操作
 
     private Bitmap mShapePic;
     private int mShapePicWidth;
     private int mShapePicHeight;
-    private Bitmap mTempBitmap;
-    private Canvas mTempCanvas;
+    private Bitmap mBottomBitmap;
+    private Canvas mBottomCanvas;
     private Canvas mSurfaceCanvas;                  //画布
+    private DrawStack mDownStack, mMoveStack, mUpStack;
 
     private int mDrawShape;                         //绘制图形
     public static final int SHAPE_PATH = 0;         //路径
     public static final int SHAPE_CIRCLE = 1;       //圆环
     public static final int SHAPE_RECTANGLE = 2;    //矩形
     public static final int SHAPE_CIRCLE_POINT = 4; //圆点
-    public static final int SHAPE_PIC = 5;          //图片
+    public static final int SHAPE_PIC_CAST = 5;     //撒花
+    public static final int SHAPE_PIC_INSERT = 6;   //插入图片
+    public static final int SHAPE_PIC_PICK = 7;     //抠图
+    public static final int SHAPE_PIC_MARK = 7;     //马赛克
 
+    private LinkedList<DrawStack> mDrawStacks = new LinkedList<>();
     private SurfaceHolder holder;
     private boolean hasReady;
 
@@ -83,6 +93,10 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
 
         holder = getHolder();
         holder.addCallback(this);
+
+        //透明
+        setZOrderOnTop(true);
+        holder.setFormat(PixelFormat.TRANSLUCENT);
     }
 
     public void setDrawShape(int mDrawShape) {
@@ -90,7 +104,7 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
         this.mDrawShape = mDrawShape;
     }
 
-    public void setShapePic(Bitmap bitmap) {
+    public void setCastPic(Bitmap bitmap) {
         if (bitmap == null) {
             return;
         }
@@ -100,13 +114,18 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
         this.mShapePic = bitmap;
         mShapePicWidth = mShapePic.getWidth();
         mShapePicHeight = mShapePic.getHeight();
-        mDrawShape = SHAPE_PIC;
+        mDrawShape = SHAPE_PIC_CAST;
         mDrawOperate = OPERATE_DRAW;
     }
 
     public void setDrawColor(int mDrawColor) {
         this.mDrawColor = mDrawColor;
         mDrawPaint.setColor(mDrawColor);
+    }
+
+    public void setDrawRadius(int mDrawRadius) {
+        this.mDrawRadius = mDrawRadius;
+        mDrawPaint.setStrokeWidth(mDrawRadius);
     }
 
     public void setBoardColor(int mBoardColor) {
@@ -124,12 +143,18 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
         mDrawOperate = OPERATE_DRAW;
     }
 
+    //设置撤销操作
+    public void setBackMode() {
+        mDrawOperate = OPERATE_BACK;
+    }
+
     //清屏
     public void clearBoard() {
         if (!checkCanDraw()) {
             return;
         }
-        mTempCanvas.drawColor(mBoardColor);
+        mDrawStacks.clear();
+        mBottomCanvas.drawColor(mBoardColor);
         refreshDraw();
         mDrawOperate = OPERATE_DRAW;
     }
@@ -139,6 +164,7 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
         if (!checkCanDraw()) {
             return false;
         }
+        DrawStack touchDrawStack = null;
         int action = MotionEventCompat.getActionMasked(event);
         switch (action) {
             case MotionEvent.ACTION_DOWN:
@@ -155,19 +181,24 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
                         case SHAPE_RECTANGLE:
                             break;
                         case SHAPE_CIRCLE_POINT:
+                            touchDrawStack = new DrawStack(mDrawShape);
+                            touchDrawStack.setDownPoints(mDownX, mDownY);
                             mDrawPaint.setStyle(Paint.Style.FILL);
-                            mTempCanvas.drawCircle(mDownX, mDownY, mDrawRadius / 2, mDrawPaint);
+                            mBottomCanvas.drawCircle(mDownX, mDownY, mDrawRadius / 2, mDrawPaint);
                             mDrawPaint.setStyle(Paint.Style.STROKE);
                             break;
-                        case SHAPE_PIC:
+                        case SHAPE_PIC_CAST:
                             if (mShapePic != null) {
-                                mTempCanvas.drawBitmap(mShapePic, mDownX - mShapePicWidth / 2, mDownY - mShapePicHeight / 2, null);
+                                mBottomCanvas.drawBitmap(mShapePic, mDownX - mShapePicWidth / 2, mDownY - mShapePicHeight / 2, null);
                             }
                             break;
                     }
                 } else if (mDrawOperate == OPERATE_CLEAR) {
                     mClearPath.reset();
                     mClearPath.moveTo(mDownX, mDownY);
+                    mClearPaint.setStyle(Paint.Style.FILL);
+                    mBottomCanvas.drawCircle(mDownX, mDownY, mClearRadius / 2, mClearPaint);
+                    mClearPaint.setStyle(Paint.Style.STROKE);
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
@@ -183,42 +214,42 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
                     switch (mDrawShape) {
                         case SHAPE_PATH:
                             mDrawPath.lineTo(moveX, moveY);
-                            mTempCanvas.drawPath(mDrawPath, mDrawPaint);
+                            mBottomCanvas.drawPath(mDrawPath, mDrawPaint);
                             break;
                         case SHAPE_CIRCLE:
                             if (mLastDistance > 0) {
                                 mDrawPaint.setStyle(Paint.Style.FILL_AND_STROKE);
                                 mDrawPaint.setColor(mBoardColor);
-                                mTempCanvas.drawCircle(mDownX, mDownY, mLastDistance / 2 + 1, mDrawPaint);
+                                mBottomCanvas.drawCircle(mDownX, mDownY, mLastDistance / 2 + 1, mDrawPaint);
                                 mDrawPaint.setColor(mDrawColor);
                                 mDrawPaint.setStyle(Paint.Style.STROKE);
                             }
-                            mTempCanvas.drawCircle(mDownX, mDownY, distance / 2, mDrawPaint);
+                            mBottomCanvas.drawCircle(mDownX, mDownY, distance / 2, mDrawPaint);
                             break;
                         case SHAPE_RECTANGLE:
                             if (mMoveX > 0 && mMoveY > 0) {
                                 mDrawPaint.setStyle(Paint.Style.FILL_AND_STROKE);
                                 mDrawPaint.setColor(mBoardColor);
-                                mTempCanvas.drawRect(mDownX, mDownY, mMoveX, mMoveY, mDrawPaint);
+                                mBottomCanvas.drawRect(mDownX, mDownY, mMoveX, mMoveY, mDrawPaint);
                                 mDrawPaint.setColor(mDrawColor);
                                 mDrawPaint.setStyle(Paint.Style.STROKE);
                             }
-                            mTempCanvas.drawRect(mDownX, mDownY, moveX, moveY, mDrawPaint);
+                            mBottomCanvas.drawRect(mDownX, mDownY, moveX, moveY, mDrawPaint);
                             break;
                         case SHAPE_CIRCLE_POINT:
                             mMoveTempOffset += tempDistance;
                             if (mMoveTempOffset >= mMoveDrawOffset) {
                                 mMoveTempOffset = 0;
                                 mDrawPaint.setStyle(Paint.Style.FILL);
-                                mTempCanvas.drawCircle(moveX, moveY, mDrawRadius / 2, mDrawPaint);
+                                mBottomCanvas.drawCircle(moveX, moveY, mDrawRadius / 2, mDrawPaint);
                                 mDrawPaint.setStyle(Paint.Style.STROKE);
                             }
                             break;
-                        case SHAPE_PIC:
+                        case SHAPE_PIC_CAST:
                             if (mMoveTempOffset >= mMoveDrawOffset) {
                                 mMoveTempOffset = 0;
                                 if (mShapePic != null) {
-                                    mTempCanvas.drawBitmap(mShapePic, moveX - mShapePicWidth / 2, moveY - mShapePicHeight / 2, null);
+                                    mBottomCanvas.drawBitmap(mShapePic, moveX - mShapePicWidth / 2, moveY - mShapePicHeight / 2, null);
                                 }
                             }
                             mMoveTempOffset += tempDistance;
@@ -226,7 +257,7 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
                     }
                 } else if (mDrawOperate == OPERATE_CLEAR) {
                     mClearPath.lineTo(moveX, moveY);
-                    mTempCanvas.drawPath(mClearPath, mClearPaint);
+                    mBottomCanvas.drawPath(mClearPath, mClearPaint);
                 }
                 mLastDistance = distance;
                 mMoveX = moveX;
@@ -239,12 +270,31 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
                 mMoveTempOffset = 0;
                 break;
         }
+        if (touchDrawStack != null) {
+            mDrawStacks.add(touchDrawStack);
+        }
         refreshDraw();
         return true;
     }
 
     private void refreshDraw() {
-        mSurfaceCanvas.drawBitmap(mTempBitmap, 0, 0, null);
+        int stackSize = mDrawStacks.size();
+        for (int i = 0; i < stackSize; i++) {
+            DrawStack drawStack = mDrawStacks.get(i);
+            switch (drawStack.drawShape) {
+                case SHAPE_PATH:
+                    break;
+                case SHAPE_CIRCLE:
+                    break;
+                case SHAPE_RECTANGLE:
+                    break;
+                case SHAPE_CIRCLE_POINT:
+                    break;
+                case SHAPE_PIC_CAST:
+                    break;
+            }
+        }
+        mSurfaceCanvas.drawBitmap(mBottomBitmap, 0, 0, null);
         holder.unlockCanvasAndPost(mSurfaceCanvas);
     }
 
@@ -262,10 +312,10 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         hasReady = true;
-        if (mTempBitmap == null) {
-            mTempBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.RGB_565);
-            mTempCanvas = new Canvas(mTempBitmap);
-            mTempCanvas.drawColor(mBoardColor);
+        if (mBottomBitmap == null) {
+            mBottomBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.RGB_565);
+            mBottomCanvas = new Canvas(mBottomBitmap);
+            mBottomCanvas.drawColor(mBoardColor);
         }
         mSurfaceCanvas = holder.lockCanvas();
         refreshDraw();
@@ -279,6 +329,30 @@ public class DrawingBoardSurface extends SurfaceView implements SurfaceHolder.Ca
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
 
+    }
+
+
+    /**
+     * 绘制回退栈
+     */
+    static class DrawStack {
+
+        public DrawStack(int drawShape) {
+            this.drawShape = drawShape;
+        }
+
+        public void setDownPoints(int pointX, int pointY) {
+            this.pointX = pointX;
+            this.pointY = pointY;
+        }
+
+        public void addPathPoint(int pointX, int pointY) {
+            points.put(pointX, pointY);
+        }
+
+        public int drawShape;
+        public int pointX, pointY;
+        public SparseIntArray points = new SparseIntArray();
     }
 
 }
